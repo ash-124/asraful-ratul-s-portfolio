de12304b4ba78807990c0e1172681fe374761ac8
Your Name <you@example.com>
2025-03-23 23:05:11 +0600

    Refining designs to simplicity and added new fonts also created a new component for about me

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div>
+                {/* About section */}
+            </div>
+            <div>
+                {/* img section of me */}
+            </div>
+            <div>
+                {/* experience , clients , projects number */}
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
